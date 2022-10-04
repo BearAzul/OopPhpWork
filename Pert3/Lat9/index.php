c30f262a7f2c11a040c1db6bad9e7f5e53fdbb95
BearAzul <adyaabduazizulhakim@gmail.com>
2022-10-04 22:50:12 +0700

    Index dan PDF fixed

--- Pert3/Lat9/index.php
+++ Pert3/Lat9/index.php
@@ -3,9 +3,5 @@
 require_once __DIR__ . "/Controllers/Library.php";
 require_once __DIR__ . "/Models/libraryModel.php";
 
-$url = isset($_SERVER["PATH_INFO"]) ? explode("/", ltrim($_SERVER["PATH_INFO"], "/")) : "/";
 $index = new Library();
-
-if($url[0] = "/"){
-   return $index->index();
-}
+$index->index();
